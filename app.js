75ea7641ca85bc415a007baa4edf9232789fa0f9
rushi7997 <ramirushi7997@gmail.com>
2018-09-03 13:26:14 +0530

    initial commit 1

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const MongoClient = require('mongodb').MongoClient;
+const cors = require('cors');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+const events = require('./api/publicity');
+const users = require('./api/user');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const auth = require('./middleware/auth');
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+dotenv.config();
+(async () => {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    const client = await MongoClient.connect(process.env.DB);
+    const db = client.db('Genesis-18');
+    console.log('Connected to database');
+    app.use('/events', auth, events(db));
+    app.use('/users', users(db));
+
+})();
 
 module.exports = app;
